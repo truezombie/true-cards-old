0aa003eea775430da785aae62afd6517c308fd19
Oleh Ovcharenko <oleshka.ovcharenko@gmail.com>
2021-07-25 22:35:30 +0300

    fix: backend url

--- ui/src/utils/config.ts
+++ ui/src/utils/config.ts
@@ -1,5 +1,5 @@
 const CONFIG = {
-  backendUrl: 'http://3.122.59.81:3000/graphql',
+  backendUrl: 'http://161.35.212.199:3000/graphql',
   maxErrorMessages: 5,
   paginationRowsPerPage: [10, 30, 50],
 };
